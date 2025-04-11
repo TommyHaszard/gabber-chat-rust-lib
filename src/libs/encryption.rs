c6d31e68f47cf9d19eb703d01b13f90e235b6b2d
ThomasHaszard <tommy45344@gmail.com>
2025-04-11 21:55:55 +1000

    JC-40 - implemented the ratchet_encrypt and ratchet_decrypt

--- src/libs/encryption.rs
+++ src/libs/encryption.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
 use std::fmt;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{format, Debug, Formatter};
 use std::ptr::write;
-use chacha20poly1305::aead::{Aead, Payload};
+use chacha20poly1305::aead::{Aead, Buffer, Payload};
 use chacha20poly1305::{AeadCore, ChaCha20Poly1305, Nonce};
 use x25519_dalek::{PublicKey, SharedSecret, StaticSecret};
 use hmac::{Hmac, Mac};
 use sha2::{Digest, Sha256};
 use hkdf::Hkdf;
 use hmac::digest::{Key, KeyInit};
+use serde::{Deserialize, Serialize};
 // Implementation of Signals Double Ratchet Algorithm with x25519_dalek
 // https://signal.org/docs/specifications/doubleratchet
 
@@ -24,16 +25,14 @@ type CipherText = Vec<u8>;
 type HmacSha256 = Hmac<Sha256>;
 
 // Store skipped message keys -> Using the PublicKey and the message number
-struct KeyStore(HashMap<PublicKey, HashMap<MessageId, SharedSecret>>);
+struct KeyStore(HashMap<(PublicKey, MessageId), KeySecret>);
 
 impl fmt::Debug for KeyStore {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut map = f.debug_map();
         for (public_key, messages) in &self.0 {
-            let redacted_map: HashMap<_, _> = messages
-                .keys()
-                .map(|msg_id| (msg_id, "[REDACTED]"))
-                .collect();
+            let redacted_map = messages
+                .map(|msg_id| (msg_id, "[REDACTED]"));
 
             map.entry(&public_key, &redacted_map);
         }
@@ -57,7 +56,10 @@ pub enum DoubleRatchetError {
     OutputSizeError(String),
     HkdfError(String),
     AeadError(String),
-    InvalidLength(String)
+    InvalidLength(String),
+    KeyNotFoundInKeyStore(String),
+    CannotPerformMaxSkip(String),
+    InvalidState(String)
 }
 
 // Using a StaticSecret instead of Ephemeral
@@ -88,7 +90,7 @@ impl fmt::Debug for DHKeyPair {
 
 
 #[derive(Debug, Clone)]
-struct MessageHeader {
+pub(crate) struct MessageHeader {
     dh_public_key: PublicKey,
     count: Counter,
     previous_count: Counter,
@@ -102,9 +104,17 @@ impl MessageHeader {
             previous_count
         }
     }
+
+    fn serialise_header(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+        result.extend_from_slice(self.dh_public_key.as_bytes());
+        result.extend_from_slice(&self.previous_count.to_be_bytes());
+        result.extend_from_slice(&self.count.to_be_bytes());
+        result
+    }
 }
 
-struct DoubleRatchet {
+pub(crate) struct DoubleRatchet {
     // Keys
     root_key: KeySecret,
     send_chain_key: Option<KeySecret>,
@@ -132,10 +142,10 @@ struct DoubleRatchet {
 
 impl DoubleRatchet {
     // Takes an initial shared secrete between two parties, the other parties public key and
-    pub fn initialise_alice(shared_root_key: KeySecret, bobby_public_key: PublicKey) {
+    pub(crate) fn initialise_alice(shared_root_key: KeySecret, bobby_public_key: PublicKey) -> DoubleRatchet {
         let dh_pair = DHKeyPair::generate_dh();
         let dh = dh_pair.private.diffie_hellman(&bobby_public_key);
-        let (rk, cks, header_key) = kdf_rk(shared_root_key, &dh).unwrap();
+        let (rk, cks, _) = kdf_rk(shared_root_key, &dh).unwrap();
 
         Self {
             root_key: rk,
@@ -148,10 +158,10 @@ impl DoubleRatchet {
             prev_number: 0,
             skipped_message_keys: KeyStore(Default::default()),
             max_skip: 0,
-        };
+        }
     }
 
-    pub fn initialise_bob(shared_root_key: KeySecret, dh_pair: DHKeyPair, cks: Option<KeySecret>) {
+    pub(crate) fn initialise_bob(shared_root_key: KeySecret, dh_pair: DHKeyPair, cks: Option<KeySecret>) -> DoubleRatchet {
         Self {
             root_key: shared_root_key,
             send_chain_key: cks,
@@ -163,20 +173,98 @@ impl DoubleRatchet {
             prev_number: 0,
             skipped_message_keys: KeyStore(Default::default()),
             max_skip: 0,
-        };
+        }
     }
 
-    pub fn ratchet_encrypt(&mut self, plaintext: &[u8], associated_data: &[u8]) -> Result<(MessageHeader, Vec<u8>), DoubleRatchetError> {
+    pub(crate) fn ratchet_encrypt(&mut self, plaintext: &[u8], associated_data: &[u8]) -> Result<(MessageHeader, Vec<u8>), DoubleRatchetError> {
         // cannot run if self is not initialised
 
-        let (new_chain_key, message_key) = kdf_ck(&self.send_chain_key.unwrap())?;
-        let header = MessageHeader::init(self.dhs.public.clone(), self.prev_number.clone(), self.send_count.clone());
+        let (_, message_key) = kdf_ck(&self.send_chain_key.unwrap())?;
+        let header = MessageHeader::init(self.dhs.public, self.prev_number, self.send_count);
 
         self.send_count += 1;
 
-        let cipher_text = encrypt(&message_key, &plaintext, &associated_data)?;
+
+        let concat_ad = concat_header_and_ad(associated_data, &header);
+
+        let cipher_text = encrypt(&message_key, plaintext, &concat_ad.as_slice())?;
 
         Ok((header, cipher_text))
+    }
+
+    pub(crate) fn ratchet_decrypt(&mut self, header: MessageHeader, cipher_text: &CipherText, associated_data: &[u8]) -> Result<Vec<u8>, DoubleRatchetError> {
+        match self.try_skipped_message_keys(&header, cipher_text, associated_data)? {
+            Some(plain_text) => Ok(plain_text),
+            None => {
+                if Some(&header.dh_public_key) != self.dhr.as_ref() {
+                    self.skip_message_keys(header.previous_count);
+                    self.dh_ratchet(&header);
+                }
+
+                self.skip_message_keys(header.count);
+                match self.recv_chain_key {
+                    Some(ckr) => {
+                        let (_, message_key) = kdf_ck(&ckr)?;
+
+                        self.recv_count += 1;
+                        let concat_ad = concat_header_and_ad(associated_data, &header);
+
+                        decrypt(&message_key, cipher_text, &concat_ad.as_slice())
+                    }
+                    _ => Err(DoubleRatchetError::InvalidState("Receive Chain Key Missing".to_string()))
+                }
+            }
+        }
+    }
+
+    fn try_skipped_message_keys(&mut self, header: &MessageHeader, cipher_text: &CipherText, associated_data: &[u8]) -> Result<Option<Vec<u8>>, DoubleRatchetError> {
+        match self.skipped_message_keys.0.remove(&(header.dh_public_key, header.count)) {
+            Some(mk) => {
+                let concat_ad = concat_header_and_ad(associated_data, header);
+                let plain_text = decrypt(&mk, cipher_text, concat_ad.as_slice())?;
+                Ok(Some(plain_text))
+            }
+            _ => Ok(None)
+        }
+    }
+
+    fn skip_message_keys(&mut self, until: u32) -> Result<(), DoubleRatchetError> {
+        if self.recv_count + (DEFAULT_MAX_SKIP as u32) < until {
+           return Err(DoubleRatchetError::CannotPerformMaxSkip(format!("{} > {}", self.recv_count+(DEFAULT_MAX_SKIP as u32), until)))
+        }
+        match self.recv_chain_key {
+            Some(ckr) =>
+                Ok(while self.recv_count < until {
+                    let (new_recv_chain_key, message_key) = kdf_ck(&ckr)?;
+                    self.skipped_message_keys.0.insert((self.dhr.unwrap(), self.recv_count), message_key);
+                    self.recv_count += 1;
+                    self.recv_chain_key = Some(new_recv_chain_key);
+                }),
+            _ => Err(DoubleRatchetError::InvalidState("Receive Chain Key Missing".to_string()))
+        }
+    }
+
+    fn dh_ratchet(&mut self, header: &MessageHeader) -> Result<(), DoubleRatchetError>{
+        self.prev_number = self.send_count;
+        self.send_count = 0;
+        self.recv_count = 0;
+        self.dhr = Some(header.dh_public_key);
+
+        // calc our DH Ratchet
+        let dh_secret = self.dhs.private.diffie_hellman(&header.dh_public_key);
+        let (new_rk, new_ckr, _) = kdf_rk(self.root_key, &dh_secret)?;
+        self.root_key = new_rk;
+        self.recv_chain_key = Some(new_ckr);
+
+        self.dhs = DHKeyPair::generate_dh();
+
+        // calc their DH Ratchet
+        let dh_secret = self.dhs.private.diffie_hellman(&header.dh_public_key);
+
+        let (new_rk, new_cks, _) = kdf_rk(self.root_key, &dh_secret)?;
+        self.root_key = new_rk;
+        self.send_chain_key = Some(new_cks);
+        Ok(())
     }
 }
 
@@ -255,7 +343,7 @@ fn encrypt(message_key: &KeySecret, plaintext: &[u8], associated_data: &[u8]) ->
     Ok(ciphertext)
 }
 
-fn decrypt(message_key: &KeySecret, cipher_text: &CipherText, associated_data: &[u8]) -> Result<CipherText, DoubleRatchetError> {
+fn decrypt(message_key: &KeySecret, cipher_text: &CipherText, associated_data: &[u8]) -> Result<Vec<u8>, DoubleRatchetError> {
     let (key_bytes, nonce_bytes) = derive_hkdf_key_and_nonce(message_key)?;
 
     let key = chacha20poly1305::Key::from_slice(&key_bytes);
@@ -288,6 +376,15 @@ fn derive_hkdf_key_and_nonce(message_key: &KeySecret) -> Result<([u8; 32], [u8; 
     Ok((key_bytes, nonce_bytes))
 }
 
+fn concat_header_and_ad(associated_data: &[u8], message_header: &MessageHeader) -> Vec<u8>{
+    let mut result = Vec::new();
+    result.extend_from_slice(associated_data);
+    let serialised_header = message_header.serialise_header();
+    result.extend_from_slice(&serialised_header);
+    result
+}
+
+
 impl fmt::Debug for DoubleRatchet {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
@@ -314,7 +411,21 @@ impl fmt::Debug for DoubleRatchet {
 
 #[cfg(test)]
 mod tests {
-    use crate::libs::encryption::*;
+    use chacha20poly1305::aead::OsRng;
+    use chacha20poly1305::aead::rand_core::RngCore;
+    use super::*;
+
+    fn init() -> (DoubleRatchet, DoubleRatchet){
+        let mut shared_key = [0u8; 32];
+        OsRng.fill_bytes(&mut shared_key);
+
+        let bob_key_pair = DHKeyPair::generate_dh();
+
+        let alice = DoubleRatchet::initialise_alice(shared_key, bob_key_pair.public);
+        let bob = DoubleRatchet::initialise_bob(shared_key, bob_key_pair, None);
+
+        (alice, bob)
+    }
 
     #[test]
     fn test_encryption_decryption() {
@@ -329,6 +440,83 @@ mod tests {
         println!("Decrypted: {:?}", String::from_utf8(pt.clone()));
         assert_eq!(pt, plaintext);
     }
+
+    #[test]
+    fn test_alice_message_one() {
+
+        let associated_data = b"TEST_ASSOCIATED_DATA";
+        let (mut alice, mut bob) = init();
+
+        let message1 = b"Hi Bob, this is Alice";
+        let (header, cipher) = alice.ratchet_encrypt(message1, associated_data).unwrap();
+
+        let plain_text = bob.ratchet_decrypt(header, &cipher, associated_data).unwrap();
+
+        println!("Decrypted: {:?}, Original: {:?}", String::from_utf8_lossy(&plain_text.clone()), String::from_utf8_lossy(&message1.clone()));
+        assert_eq!(plain_text, message1);
+    }
+
+    #[test]
+    fn test_bob_message_returned() {
+
+        let alice_ad = b"ALICE_ASSOCIATED_DATA";
+        let bob_ad = b"BOB_ASSOCIATED_DATA";
+        let (mut alice, mut bob) = init();
+
+        let message1 = b"Hi Bob, this is Alice";
+        let (header, cipher) = alice.ratchet_encrypt(message1, alice_ad).unwrap();
+
+        let plain_text = bob.ratchet_decrypt(header, &cipher, alice_ad).unwrap();
+
+        println!("Decrypted: {:?}, Original: {:?}", String::from_utf8_lossy(&plain_text.clone()), String::from_utf8_lossy(&message1.clone()));
+
+        let message2 = b"Hey Alice, how have you been?";
+        let (header, cipher) = bob.ratchet_encrypt(message2, bob_ad).unwrap();
+
+        let plain_text = alice.ratchet_decrypt(header, &cipher, bob_ad).unwrap();
+        println!("Decrypted: {:?}, Original: {:?}", String::from_utf8_lossy(&plain_text.clone()), String::from_utf8_lossy(&message2.clone()));
+        assert_eq!(plain_text, message2);
+    }
+
+    #[test]
+    fn test_alice_out_of_order() {
+        // Alice sends three messages
+        let alice_ad = b"ALICE_ASSOCIATED_DATA";
+        let messages = [
+            b"Message A (will arrive in order)",
+            b"Message B (will be dlayd aftr c)",
+            b"Message C (will arrive before B)"
+        ];
+
+        let (mut alice, mut bob) = init();
+
+        let mut headers_and_ciphertexts = Vec::new();
+
+        for msg in messages {
+            let (header, ciphertext) = alice.ratchet_encrypt(msg, alice_ad).unwrap();
+            headers_and_ciphertexts.push((header, ciphertext));
+            println!("Alice encrypted: {}", String::from_utf8_lossy(msg));
+        }
+
+        // Bob receives messages in different order (A, C, B)
+        let (header_a, ciphertext_a) = &headers_and_ciphertexts[0];
+        let plaintext_a = bob.ratchet_decrypt(header_a.clone(), ciphertext_a, alice_ad).unwrap();
+        println!("Bob received (1st): {}", String::from_utf8_lossy(&plaintext_a));
+        assert_eq!(plaintext_a, messages[0]);
+
+        // Message C arrives before B
+        let (header_c, ciphertext_c) = &headers_and_ciphertexts[2];
+        let plaintext_c = bob.ratchet_decrypt(header_c.clone(), ciphertext_c, alice_ad).unwrap();
+        println!("Bob received (2nd): {}", String::from_utf8_lossy(&plaintext_c));
+        assert_eq!(plaintext_c, messages[2]);
+
+        // Finally, message B arrives
+        let (header_b, ciphertext_b) = &headers_and_ciphertexts[1];
+        let plaintext_b = bob.ratchet_decrypt(header_b.clone(), ciphertext_b, alice_ad).unwrap();
+        println!("Bob received (3rd): {}", String::from_utf8_lossy(&plaintext_b));
+        assert_eq!(plaintext_b, messages[1]);
+    }
+
 
     #[test]
     fn test_gen_user_key() {
